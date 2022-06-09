cb981a9f4b56f9f7c9ea2fd2e37554dbd6296bbb
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-06-09 11:15:53 -0400

    Implemented initial (base) service layer for posts

---
+++ src/main/java/org/wallees/blogwebsite/service/PostServiceImpl.java
@@ -0,0 +1,37 @@
+package org.wallees.blogwebsite.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.wallees.blogwebsite.model.Post;
+import org.wallees.blogwebsite.repository.PostRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Override
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+    @Override
+    public Post getPostById(Long id) {
+        Optional<Post> optional = postRepository.findById(id);
+        Post post = null;
+        if (optional.isPresent()) {
+            post = optional.get();
+        } else {
+            throw new RuntimeException("Post not found.");
+        } return post;
+    }
+
+    @Override
+    public void savePost(Post post) {
+        this.postRepository.save(post);
+    }
+}
