e83051d6c2cd29c9ffb330161841d0b1e19470d5
ambamxlc <amber.cruz5@gmail.com>
2022-06-09 16:26:38 -0400

    backend

---
+++ src/main/java/org/wallees/blogwebsite/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.wallees.blogwebsite.repository;
+
+import org.wallees.blogwebsite.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ src/main/java/org/wallees/blogwebsite/service/UserService.java
@@ -0,0 +1,15 @@
+package org.wallees.blogwebsite.service;
+
+import org.wallees.blogwebsite.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+List<User> findAll();
+User findById(Long id);
+User create(User user);
+User edit(User user);
+void deleteById(Long id);
+
+}
