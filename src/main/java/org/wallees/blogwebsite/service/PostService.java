874644d5379a09e20daf91ddc92f0ba0d69d50d6
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-06-09 11:15:20 -0400

    Created service layer interface for posts (with functionality for getting all, getting single post and saving post to repository)

---
+++ src/main/java/org/wallees/blogwebsite/service/PostService.java
@@ -0,0 +1,12 @@
+package org.wallees.blogwebsite.service;
+
+import org.wallees.blogwebsite.model.Post;
+
+import java.util.List;
+
+public interface PostService {
+
+    List<Post> getAllPosts();
+    Post getPostById(Long id);
+    void savePost(Post post);
+}
