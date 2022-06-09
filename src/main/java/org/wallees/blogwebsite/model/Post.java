960aac25a34789d76247bdbfd6e029fd8da339d8
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-06-09 10:40:06 -0400

    Created initial structure for 'Post' entity

---
+++ src/main/java/org/wallees/blogwebsite/model/Post.java
@@ -0,0 +1,65 @@
+package org.wallees.blogwebsite.model;
+
+import javax.persistence.*;
+
+//Model for each individual blog post
+@Entity
+@Table
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private int userId;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "body", nullable = false)
+    private String body;
+
+    @Column(name = "date", nullable = false)
+    private String date;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
