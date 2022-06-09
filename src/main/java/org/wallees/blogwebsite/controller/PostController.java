d36edb2a1925e42a8984cc9eaf66bd07ce28e1b2
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-06-09 13:21:56 -0400

    Initial commit

---
+++ src/main/java/org/wallees/blogwebsite/controller/PostController.java
@@ -0,0 +1,13 @@
+package org.wallees.blogwebsite.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.wallees.blogwebsite.repository.PostRepository;
+
+public class PostController {
+
+    @Autowired
+    PostRepository postRepository;
+
+
+
+}
