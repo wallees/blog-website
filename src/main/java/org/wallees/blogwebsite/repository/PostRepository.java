fa224293adb2ef854ae8c50f0f529e37a2c85f07
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-06-09 14:22:48 -0400

    Removed unnecessary import

--- src/main/java/org/wallees/blogwebsite/repository/PostRepository.java
+++ src/main/java/org/wallees/blogwebsite/repository/PostRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.wallees.blogwebsite.model.Post;
 
-import java.util.Collection;
-
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
