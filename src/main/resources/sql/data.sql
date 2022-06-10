ffdd189b8b63327a17503aa54a942774c8d1e17f
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-06-10 09:55:34 -0400

    Added some pre-loading data for posts table

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,2 +1,18 @@
-CREATE DATABASE wallees;
-USE wallees;
+DROP TABLE IF EXISTS posts;
+CREATE TABLE posts (
+                       id int,
+                       user_id int,
+                       title varchar(50),
+                       body varchar (255),
+                       date datetime
+);
+INSERT INTO posts (id, user_id, title, body)
+VALUES (1, 1, 'Developer Dynamic', 'The CSV Linux is down, secure the public environment so we can chmod the RPC shareware! We need to transact the atomic SQL domain! So gate-keeping the promise won''t do anything, we need to promise the native FP flexbox! We need to integrate the private DOM pattern! Use the atomic CSS-in-JS freelancer, then you can compile the private Linux! Try to bundle the RPC Firefox, maybe it will transact the test-driven program!');
+INSERT INTO posts (id, user_id, title, body)
+VALUES (2, 1, 'Gulp Sort', 'So code-splitting the library won''t do anything, we need to lazy eval the greenfield LIFO convention!');
+INSERT INTO posts (id, user_id, title, body)
+VALUES (3, 2, 'CSS grid', 'Queue val Chrome bubble sort key-value callback hell. Composition documentation driven IoT stack .NET gradle FIFO. Domain YAML UX S3 npm frame rate bitcoin Kubernetes.');
+INSERT INTO posts (id, user_id, title, body)
+VALUES (4, 3, 'Injection Hipchat', 'Hacker News heap sort free as speech contribution killer app architecture. Cherry pick greenfield production gradle program Cloudfront. Greedy algorithm static typing directed acyclic graph behavior-driven dog-piling Edge domain ICO a place for everything. Composition over inheritance machine learning stand-up contributor first in first out ping queue font legacy.');
+INSERT INTO posts (id, user_id, title, body)
+VALUES (5, 2, 'Asynchronous Team-Player', 'Fullstack design waterfall Github duck typing a place for everything JVM view-model CSV. Ubuntu developer callback hell DSL YAML flexbox middleware killer app. Keycaps domain font native convention static progressive web app.');
