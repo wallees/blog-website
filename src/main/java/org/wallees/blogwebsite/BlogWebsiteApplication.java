e61da2358fe7763a6dd3d14fc6ffaf81f381eb8d
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-06-13 11:08:51 -0400

    Resolving warnings

--- src/main/java/org/wallees/blogwebsite/BlogWebsiteApplication.java
+++ src/main/java/org/wallees/blogwebsite/BlogWebsiteApplication.java
@@ -2,7 +2,6 @@ package org.wallees.blogwebsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class BlogWebsiteApplication {
@@ -10,5 +9,4 @@ public class BlogWebsiteApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BlogWebsiteApplication.class, args);
 	}
-
 }
