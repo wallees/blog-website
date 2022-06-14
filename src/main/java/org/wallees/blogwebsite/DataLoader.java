dbc029e0dd3feaba832113ab328e810f272d481a
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-06-14 09:33:57 -0400

    Updated preloaded data to use User user's rather than int userId's

--- src/main/java/org/wallees/blogwebsite/DataLoader.java
+++ src/main/java/org/wallees/blogwebsite/DataLoader.java
@@ -6,6 +6,7 @@ import org.wallees.blogwebsite.model.Post;
 import org.wallees.blogwebsite.model.User;
 import org.wallees.blogwebsite.repository.PostRepository;
 import org.wallees.blogwebsite.repository.UserRepository;
+import org.wallees.blogwebsite.service.UserService;
 
 @Component
 public class DataLoader {
@@ -14,30 +15,15 @@ public class DataLoader {
 
     private UserRepository userRepository;
 
+    private UserService userService;
+
     @Autowired
-    public DataLoader(PostRepository postRepository, UserRepository userRepository){
+    public DataLoader(PostRepository postRepository, UserRepository userRepository, UserService userService){
         this.postRepository = postRepository;
         this.userRepository = userRepository;
-        LoadPosts();
+        this.userService = userService;
         LoadUsers();
-    }
-
-    private void LoadPosts(){
-        postRepository.save(new Post(
-                1, "Developer Dynamic", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        ));
-        postRepository.save(new Post(
-                2, "Sorted Craze", "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-        ));
-        postRepository.save(new Post(
-                1, "CSS grid", "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-        ));
-        postRepository.save(new Post(
-                3, "Injection Hipchat", "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        ));
-        postRepository.save(new Post(
-                3, "Asynchronous Team-Player", "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore."
-        ));
+        LoadPosts();
     }
 
     private void LoadUsers(){
@@ -47,4 +33,23 @@ public class DataLoader {
         userRepository.save(new User("creale", "creale@gmail.com", "$2a$10$/3HV/B4dOlx3nPpDZPG.KeYxS/AcHtTync2rfeC3ZynwVeOdDLsXW", "Colin", "Reale"));
         userRepository.save(new User("janderson", "janderson@gmail.com", "$2a$10$/3HV/B4dOlx3nPpDZPG.KeYxS/AcHtTync2rfeC3ZynwVeOdDLsXW", "Joseph", "Anderson"));
     }
+
+    private void LoadPosts(){
+        postRepository.save(new Post(
+                userService.findByUsername("acruz"), "Developer Dynamic", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        ));
+        postRepository.save(new Post(
+                userService.findByUsername("prallings"), "Sorted Craze", "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        ));
+        postRepository.save(new Post(
+                userService.findByUsername("creale"), "CSS grid", "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+        ));
+        postRepository.save(new Post(
+                userService.findByUsername("prallings"), "Injection Hipchat", "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        ));
+        postRepository.save(new Post(
+                userService.findByUsername("janderson"), "Asynchronous Team-Player", "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore."
+        ));
+    }
+
 }
