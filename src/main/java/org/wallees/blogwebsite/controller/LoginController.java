15937aff2154428cf911f61763d4560a18119edf
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-06-15 09:28:16 -0400

    Removed redundant redirect (already set in SecurityConfiguration)

--- src/main/java/org/wallees/blogwebsite/controller/LoginController.java
+++ src/main/java/org/wallees/blogwebsite/controller/LoginController.java
@@ -1,6 +1,5 @@
 package org.wallees.blogwebsite.controller;
 
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,11 +8,6 @@ public class LoginController {
 
     @GetMapping("/login")
     public String login() {
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        if (!username.equals("anonymousUser")) {
-            return "redirect:/";
-        } else {
             return "login";
         }
-    }
 }
