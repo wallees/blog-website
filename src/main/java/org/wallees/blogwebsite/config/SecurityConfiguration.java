0847c615e145072a8287e0d2e53a4032c15b974f
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-06-15 09:25:05 -0400

    Added success message upon logging in

--- src/main/java/org/wallees/blogwebsite/config/SecurityConfiguration.java
+++ src/main/java/org/wallees/blogwebsite/config/SecurityConfiguration.java
@@ -35,6 +35,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login")
+                .defaultSuccessUrl("/?login", true)
                 .permitAll()
                 .and()
                 .logout()
