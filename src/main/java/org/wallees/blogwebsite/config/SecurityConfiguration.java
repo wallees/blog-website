44c4b71b9cea40f9a5bce6c9b84c40494c92776e
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-06-16 16:46:33 -0400

    Updated in tandem with file structure update

--- src/main/java/org/wallees/blogwebsite/config/SecurityConfiguration.java
+++ src/main/java/org/wallees/blogwebsite/config/SecurityConfiguration.java
@@ -27,10 +27,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                         "/",
                         "/register**",
                         "/posts**",
-
                         "/js/**",
                         "/css/**",
-                        "/img/**",
+                        "/images/**",
                         "/webjars/**")
                 .permitAll()
                 .anyRequest().authenticated()
